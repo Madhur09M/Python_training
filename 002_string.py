da12f4489d6700ab0354686fba30f70baafe0baa
Madhur09M <117634573+Madhur09M@users.noreply.github.com>
2025-06-09 20:26:02 +0530

    Add files via upload

---
+++ 002_string.py
@@ -0,0 +1,28 @@
+# name = "Anshu is a girl"
+# print(name)
+# print(type(name))
+
+# print(name.swapcase())
+
+# print(name.strip())
+# print(name.lstrip())
+# print(name.rstrip())
+
+# print(name.replace("girl","boy"))
+
+# print(name.split(" "))
+
+# print(name.partition(" "))
+
+# print(name.zfill(20))
+# print(name.center(20,"%"))
+# print(name.rjust(20,"$"))
+# print(name.ljust(20,"*"))
+
+name = "Anshu is a girl"
+
+# print(name[0])     #positive indexing
+# print(name[-1])    #negative indexing
+
+# print(name[0:5])
+print(name[-4:])
